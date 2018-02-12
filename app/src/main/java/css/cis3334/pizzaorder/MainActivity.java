95f0dcb98c11423e292d35f2f0255f9b160566fa
s-poverud <32241623+s-poverud@users.noreply.github.com>
2018-02-11 19:37:29 -0600

    asg4 commit

--- app/src/main/java/css/cis3334/pizzaorder/MainActivity.java
+++ app/src/main/java/css/cis3334/pizzaorder/MainActivity.java
@@ -60,8 +60,25 @@ public class MainActivity extends AppCompatActivity implements updateViewInterfa
     public void onClickOrder(View view) {
         // ****** Students need to add code here to get information from the UI widgets...
 
+        //set delivery
+        pizzaOrderSystem.setDelivery(chkbxDelivery.isChecked());
+        String size;
+        if(rbSmall.isChecked()){
+            size = "small";
+        }else if(rbMedium.isChecked()){
+            size = "medium";
+        }else if(rbLarge.isChecked()){
+            size = "large";
+        }else{
+            //throw ex
+        }
+        
         // ****** Students need to modify the call to OrderPizza to order the type of pizza the user selects using the UI widgets
-        String orderDescription = pizzaOrderSystem.OrderPizza("Peperoni", "large", false  );
+        String orderDescription = pizzaOrderSystem.OrderPizza(
+            spinnerToppings.getSelectedItem().toString(),
+            size,
+            chkbxCheese.isChecked()
+        );
 
 
         //display a pop up message for a long period of time
